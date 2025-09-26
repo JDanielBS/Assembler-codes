d5e4a36126ee099be0222f12f43c78fd94a11f79
Liceth Usma <juanitausma02@gmail.com>
2025-09-26 14:36:58 -0500

    fix(Taller-io): soluciona el punto 2

--- Taller IO (no esta bien hecho todo)/Punto 2/main.asm
+++ Taller IO (no esta bien hecho todo)/Punto 2/main.asm
@@ -1,118 +1,101 @@
 ;
-; AssemblerApplication11.asm
+; Taller_1_punto_2_sensores.asm
 ;
-; Created: 9/3/2025 13:48:22
-; Author : Liceth Usma
+; Created: 02/09/2025 21:15:14
+; Author : jonat
 ;
-
-
+; ontrol de depósito de agua
+; PORTF[3:0] = sensores (switches)
+; PORTK[3:0] = barra de nivel
+; PORTK4     = bomba (ON/OFF)
+; PORTK5     = tanque lleno (parpadeo doble)
 ; Replace with your application code
-;
-; AssemblerApplication9.asm
-;
-; Created: 8/3/2025 21:22:44
-; Author : Liceth Usma
-;
 
 
-; Replace with your application code
-;
-; AssemblerApplication8.asm
-;
-; Created: 8/3/2025 04:28:45
-; Author : Liceth Usma
-;
-
-
-; Replace with your application code
-;
-; AssemblerApplication6.asm
-;
-; Created: 5/3/2025 00:13:57
-; Author : Liceth Usma
-;
-
-
-; Replace with your application code
 start:
-    ldi r17, 0b00001111  
-    ldi r18, 0b11110000  
-    ldi r19, 0b11111100
-    out ddrf, r17
-    out ddra, r19
-    sts ddrk, r18
-    ser r21
-    ldi r23, 2
-    ldi r24, 1
-    ldi r25, 1
+    ; Configurar Puerto F como entrada (sensores)
+    ldi r16, 0x00
+    out DDRF, r16
 
-comparar:
-	clr r18
-	clr r19
-    in r17, pinf        
-    cpi r17, 0          
-    breq bomba_agua     
-    cpi r17, 1          
-    breq bomba_agua     
-    brne cambiar_valor
+    ; Configurar Puerto K como salida (LEDs + bomba)
+    ldi r16, 0xFF
+    sts DDRK, r16
 
-cambiar_valor:
-	ldi r25, 1
-	ldi r20, 1
-	rjmp nivel
+    ; Inicializar estado de bomba en 0 (apagada)
+    ldi r21, 0x00
 
-bomba_agua:
-	cpi r25, 0
-	mov r18, r17
-	breq mostrar_nivel
-    ldi r23, 0b00000001  
-    ldi r25, 0
-llenando:
-	or r19, r23          
-	out PORTA, r24       
-	sts PORTk, r19      
-	cpi r19, 0b00001111  
-	breq mostrar_nivel   
-    lsl r23              
-    rjmp llenando        
+main_loop:
+    ; Leer sensores
+    in r17, PINF
+    andi r17, 0x0F        ; Solo 4 bits bajos
 
-nivel:
-    cpi r17, 0
-    breq mostrar_nivel
-    lsr r17
-    inc r18
-	or r19, r20
-    lsl r20
-    rjmp nivel
+    ; Mostrar nivel en barra de LEDs (K0..K3)
+    mov r18, r17
+    sts PORTK, r18
 
-mostrar_nivel:
-    mov r21, r18
+    ; ----- Control de bomba -----
+    ; Nivel máximo -> apagar bomba y parpadear LED max
+    cpi r17, 0x0F
+    breq bomba_off
+
+    ; Si la bomba ya está encendida, mantenerla
     cpi r21, 1
-    breq nivel_maximo
-	cpi r21, 0
-    breq nivel_maximo
-	sts PORTk, r19  
-    cpi r21, 4
-    breq nivel_maximo
-    rjmp comparar   
+    breq bomba_keep
+
+    ; Si estamos en nivel mínimo (0001), encender bomba
+    cpi r17, 0x01
+    breq bomba_on
+
+    ; En cualquier otro caso ? bomba apagada
+    rjmp bomba_idle
 
 
-nivel_maximo:
-    ldi r23, 2  
+; ----------- Rutinas de control -----------
+bomba_on:
+    ldi r21, 1            ; marcar estado ON
+    lds r18, PORTK
+    ori r18,  0x10       ; encender LED bomba (PK4)
+    andi r18, 0b11011111  ; asegurar que PK5 apagado
+    sts PORTK, r18
+    rjmp main_loop
 
-parpadeo:
-    ldi r22, 2
-    out PORTA, r22   
-	clr r22
-    out PORTA, r22  
-    dec r23         
-    brne parpadeo    
+bomba_keep:
+    ; mantener bomba encendida mientras no llegue al máximo
+    lds r18, PORTK
+    ori r18,  0x10         ; PK4 = ON
+    andi r18, 0b11011111  ; PK5 apagado
+    sts PORTK, r18
+    rjmp main_loop
 
-    rjmp comparar    
+bomba_off:
+    ldi r21, 0            ; marcar estado OFF
+    lds r18, PORTK
+    andi r18, 0b11101111  ; apagar PK4
+    sts PORTK, r18
+
+    ; Parpadear LED máximo en PK5
+    lds r18, PORTK
+    ldi r19, (1<<5)
+    eor r18, r19          ; toggle PK5
+    sts PORTK, r18
+
+    rcall delay
+    rjmp main_loop
+
+bomba_idle:
+    ; bomba apagada sin parpadeo (PK4 y PK5 = 0)
+    lds r18, PORTK
+    andi r18, 0b11001111
+    sts PORTK, r18
+    rjmp main_loop
 
 
-
-
-
-
-
+; ---------- Rutina de retardo ----------
+delay:
+    ldi r19, 100
+d1: ldi r20, 255
+d2: dec r20
+    brne d2
+    dec r19
+    brne d1
+    ret
